2ae996f737d7cf5e0d3686ee4e7c719cbfca3bd1
John Dave Decano <johndavedecano@gmail.com>
2020-11-05 04:53:56 +0800

    fix grid columns

--- src/pages/instructor/index.js
+++ src/pages/instructor/index.js
@@ -9,7 +9,7 @@ const Component = () => {
     <Layout>
       <div className="p-6">
         <h3 className="text-3xl mb-3">Students</h3>
-        <div className="grid grid-flow-col grid-rows-3 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-4">
           {students.map((_, i) => {
             return <Student key={i} id={i} />;
           })}
